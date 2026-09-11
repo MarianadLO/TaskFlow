9bc96f7c5d4d2fef22a8ed963a4d69d73407e7d6
MarianadLO <https://github.com/MarianadLO>
2026-09-10 22:39:51 -0300

    inserção do display funcional das tarefas concluídas

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,24 +1,36 @@
+// ===== REFERÊNCIAS AO HTML =====
 const form = document.querySelector("#form-area");
 const listaTarefas = document.querySelector("#task-list");
 const tarefaInput = document.querySelector("#tarefa_titulo");
 const categoriaSelect = document.querySelector("#tarefa_categoria");
 const tarefaSelect = document.querySelector("#tarefa_prioridade");
+const totalTarefas =document.querySelector("#total-tarefas");
+const tarefas = [];
 
 
 form.addEventListener("submit", function(event){
 
     event.preventDefault();
 
+     // ===== VALORES PREENCHIDOS PELO USUÁRIO =====
     const titulo = tarefaInput.value;
     const categoria = categoriaSelect.value;
     const prioridade = tarefaSelect.value;
 
+    // ===== REPRESENTAÇÃO DE UMA TAREFA =====
     const novaTarefa = {
         titulo,
         categoria,
         prioridade,
+        concluida:false,
     };
 
+    // ======== ADIÇÃO DE NOVA TAREFA AO ARRAY E INSERÇÃO NO DISPLAY DE TAREFAS =========
+
+    tarefas.push(novaTarefa);
+    totalTarefas.textContent = tarefas.length;
+
+    // ======== CRIAÇÃO DO CARD DE TAREFA ========
     const li = document.createElement("li");
     const mainTask = document.createElement("div");
     const input = document.createElement("input");
@@ -27,27 +39,42 @@ form.addEventListener("submit", function(event){
     const spanPrioridade = document.createElement("span");
     const spanCategoria = document.createElement("span");
 
+    if (novaTarefa.prioridade === "Alta") {
+        spanPrioridade.classList.add("alta");
+    } else if (novaTarefa.prioridade === "Média") {
+        spanPrioridade.classList.add("media");
+    } else if (novaTarefa.prioridade === "Baixa") {
+        spanPrioridade.classList.add("baixa");
+    };
+
+    // ======== ADIÇÃO DAS CLASSES CSS AOS ELEMENTOS DO CARD ========
     mainTask.classList.add("main-task");    
     infoTask.classList.add("info-task");
 
-
+    
     input.type="checkbox";
     h2.textContent = novaTarefa.titulo;
     spanCategoria.textContent = novaTarefa.categoria;
     spanPrioridade.textContent = novaTarefa.prioridade;
+    spanPrioridade.classList.add("prioridade");
 
+    // =========== CONSTRUÇÃO DO CARD =========
     mainTask.appendChild(input);
     mainTask.appendChild(h2);
-
+    
     infoTask.appendChild(spanCategoria);
     infoTask.appendChild(spanPrioridade);
-
+    
     li.appendChild(mainTask);
     li.appendChild(infoTask);
-
-    // 6. Jogando o card completo na tela e limpando o formulário
+    
+    // ==== Jogando o card completo na tela e limpando o formulário =========
     listaTarefas.appendChild(li);
     form.reset();
 
-    console.log("Seu formulário foi enviado com sucesso.")
+
+    console.log("Seu formulário foi enviado com sucesso.");
+
+    
+
 })
